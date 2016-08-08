b8c737dd940f3a5ea436e11d978e81d285325613
mtasota <miketasota@gmail.com>
2016-08-08 15:53:05 -0400

    Added 'values' accessor for DataPoint (plus callbacks) to handle Field key-values.

--- app/models/data_point.rb
+++ app/models/data_point.rb
@@ -5,16 +5,62 @@ class DataPoint < ActiveRecord::Base
 
   validates :channel, :latitude, :longitude, :presence => true
 
+  validate :contains_all_required_fields
+  validate :contains_only_defined_fields
+  after_save :save_field_values
 
-  # construct a hash with field names as Keys along with their associated value
+  attr_accessor :values
+
   def values
-    result = {}
-
-    self.data_point_field_values.each do |field_value|
-      result["#{field_value.field.name}"] = field_value.value
+    if @values.nil?
+      @values = {}
+      self.data_point_field_values.each do |field_value|
+        @values["#{field_value.field.name}"] = field_value.value
+      end
     end
-
-    return result
+    @values
   end
+
+
+  # class methods
+  #
+
+  private
+  def contains_all_required_fields
+    required_fields = self.channel.fields.where(:is_required => true).map{|f| f.name.downcase}
+    keys = self.values.keys.map(&:downcase)
+    unless required_fields.to_set.subset?(keys.to_set)
+      missing_fields = (required_fields - keys).join(", ")
+      errors.add(:data_point_field_values, "required field(s) missing: #{missing_fields}.")
+      return
+    end
+  end
+
+  def contains_only_defined_fields
+    channel_fields = self.channel.fields.map{|f| f.name.downcase}
+    keys = self.values.keys.map(&:downcase)
+    unless keys.to_set.subset?(channel_fields.to_set)
+      missing_fields = (keys - channel_fields).join(", ")
+      errors.add(:data_point_field_values, "undefined field(s): #{missing_fields}.")
+      return
+    end
+  end
+
+  def save_field_values
+    # destroy all old entries
+    self.data_point_field_values.destroy_all
+
+    self.values.keys.each do |key|
+      # create DB entry for each key-value pair
+      row = DataPointFieldValue.new
+      row.data_point = self
+      # ASSERT: this should exist, if we were validated
+      row.field = Field.where(:channel_id => self.channel_id, :name => key).first
+      row.value = self.values[key].to_s
+      row.save
+    end
+  end
+
+  # private :contains_all_required_fields, :contains_only_defined_fields, :save_field_values
 
 end
