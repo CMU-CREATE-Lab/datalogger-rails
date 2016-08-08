8b696b8482172829998ac44b6fa83ea408869e54
mtasota <miketasota@gmail.com>
2016-08-08 13:30:32 -0400

    Added some unit tests, plus helper method to generate a hash of values for a DataPoint.

--- app/models/data_point.rb
+++ app/models/data_point.rb
@@ -5,4 +5,16 @@ class DataPoint < ActiveRecord::Base
 
   validates :channel, :latitude, :longitude, :presence => true
 
+
+  # construct a hash with field names as Keys along with their associated value
+  def values
+    result = {}
+
+    self.data_point_field_values.each do |field_value|
+      result["#{field_value.field.name}"] = field_value.value
+    end
+
+    return result
+  end
+
 end
--- test/models/channel_test.rb
+++ test/models/channel_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class ChannelTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @testChannel = channels(:testChannel)
+  end
+
+  test "channel and fields exist" do
+    assert not(@testChannel.fields.blank?)
+  end
+
+  test "all data points have required fields" do
+    Channel.all.each do |channel|
+      required_fields = channel.fields.where(:is_required => true)
+      channel.data_points.each do |datapoint|
+        fields = datapoint.data_point_field_values.map(&:field)
+        assert false unless required_fields.to_set.subset? fields.to_set
+      end
+    end
+
+    assert true
+  end
+
 end
