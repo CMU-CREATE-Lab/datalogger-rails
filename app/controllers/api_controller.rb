e6d3217f75f6eb4716c4c247c96d31f8627c8548
mtasota <miketasota@gmail.com>
2016-08-09 10:50:41 -0400

    Added code for Channel create.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,7 @@
 class ApiController < ApplicationController
 
+  skip_before_action :verify_authenticity_token
+
   # channels
 
   # POST /api/v1/channels
@@ -7,9 +9,52 @@ class ApiController < ApplicationController
   # PARAMS
   # "name" : String *
   # "description" : String
-  # "fields" : Array:[ {"type" : FieldType *, "name" : String *, "description" : String, "is_required" : Boolean }[,{...}] ]
+  # "fields" : Array:[ {"field_type" : FieldType *, "name" : String *, "description" : String, "is_required" : Boolean }[,{...}] ]
   #
   def channel_create
+    channel = Channel.new
+    channel.name = params["name"] unless params["name"].blank?
+    channel.description = params["description"] unless params["description"].blank?
+    fields = params["fields"].blank? ? [] : params["fields"].to_a
+
+    if Field.valid_channel_fields_array?(fields)
+      result_fields = []
+      begin
+        channel.save!
+
+        fields.each do |f|
+          field = Field.new
+          result_fields.push field
+
+          field.name = f["name"] unless f["name"].blank?
+          field.field_type = f["field_type"] unless f["field_type"].blank?
+          field.description = f["description"] unless f["description"].blank?
+          field.is_required = f["is_required"] unless f["is_required"].blank?
+          field.channel = channel
+          field.save!
+        end
+
+        response = {
+          :channel_id => channel.id,
+          :name => channel.name,
+          :description => channel.description,
+          :fields => channel.fields.map{|f| JSON.parse(f.to_json(:only => [:name,:description,:field_type,:is_required]))}
+        }
+      rescue Exception => e
+        # make sure we delete any records that might have been created
+        channel.destroy
+        result_fields.each(&:destroy)
+        response = {
+          :error => e.message
+        }
+      end
+    else
+      response = {
+        :error => "Defined fields are invalid."
+      }
+    end
+
+    render :json => response, :layout => false
   end
 
   # GET /api/v1/channels
--- app/models/field.rb
+++ app/models/field.rb
@@ -13,4 +13,22 @@ class Field < ActiveRecord::Base
   validates :field_type, :inclusion => { :in => VALID_FIELD_TYPES, :message => "%{value} is not a valid Field type."}
   validates :name, :uniqueness => { :case_sensitive => false, :scope => :channel, :message => "Fields in the same channel must have unique names." } # TODO validate format as well?
 
+
+  def self.valid_channel_fields_array?(channel_fields)
+    defined_fields = []
+    return false if channel_fields.blank?
+
+    channel_fields.each do |field|
+      return false if field["field_type"].blank? or field["name"].blank?
+      return false unless VALID_FIELD_TYPES.include? field["field_type"]
+      if defined_fields.include? field["name"].downcase
+        return false
+      else
+        defined_fields.push field["name"].downcase
+      end
+    end
+
+    return true
+  end
+
 end
