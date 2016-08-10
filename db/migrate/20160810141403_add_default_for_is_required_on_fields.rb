f17fa724b6aa2b371055fd2f07bc605cc699227c
mtasota <miketasota@gmail.com>
2016-08-10 13:48:35 -0400

    Fixed migration to work on server (didn't like the other change column method)

--- db/migrate/20160810141403_add_default_for_is_required_on_fields.rb
+++ db/migrate/20160810141403_add_default_for_is_required_on_fields.rb
@@ -1,6 +1,9 @@
 class AddDefaultForIsRequiredOnFields < ActiveRecord::Migration
   def change
-    change_column_null :fields, :is_required, false
     change_column_default :fields, :is_required, false
+    Field.where(:is_required => nil).each do |field|
+      field.is_required = false
+      field.save
+    end
   end
 end
